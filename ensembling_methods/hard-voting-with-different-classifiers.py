c3b6af34d7e78fe8fafe72111175c5c0e8be0332
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-11-24 15:11:45 +0530

    added voting classifiers

--- ensembling_methods/hard-voting-with-different-classifiers.py
+++ ensembling_methods/hard-voting-with-different-classifiers.py
@@ -1,3 +1,6 @@
+# Ensemble methods work best when the predictors are as independent from one another as possible. One way to get diverse classifiers is to train them using very different algorithms. This increases the
+chance that they will make very different types of errors, improving the ensemble’s accuracy.
+
 from sklearn import datasets
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -14,15 +17,14 @@ y_data = data.target   # load y_data
 X_data = X_data.reshape(X_data.shape[0], X_data.shape[1] * X_data.shape[2])    # flatten X_data
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size = 0.2, random_state = 7)    # split data into train & test set
 
-clf_1 = SVC(probability=True)    # probability has to be set 'True' in order to perform soft voting
-clf_2 = DecisionTreeClassifier()
-clf_3 = GaussianNB()
+clf_svc = SVC() 
+clf_dt = DecisionTreeClassifier()
+clf_gnb = GaussianNB()
 
-soft_vote_classifier = VotingClassifier(estimators = [('svm', clf_1), ('decision_tree', clf_2), ('naive_bayes', clf_3)], voting = 'soft')
-soft_vote_classifier.fit(X_train, y_train)
+voting_clf = VotingClassifier(estimators = [('svm', clf_svc), ('decision_tree', clf_dt), ('naive_bayes', clf_gnb)], voting = 'hard')
+voting_clf.fit(X_train, y_train)
 
-
-for clf in (clf_1, clf_2, clf_3, soft_vote_classifier):
+for clf in (clf_svc, clf_dt, clf_gnb, voting_clf):
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
