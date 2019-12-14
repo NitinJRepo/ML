d443b016ee6c0be3c88d6feadb1dc986a7a329a0
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-12-14 12:17:22 +0530

    hard voting with different classifiers

--- ensembling_methods/hard-voting-with-different-classifiers.py
+++ ensembling_methods/hard-voting-with-different-classifiers.py
@@ -1,5 +1,5 @@
-# Ensemble methods work best when the predictors are as independent from one another as possible. One way to get diverse classifiers is to train them using very different algorithms. This increases the
-chance that they will make very different types of errors, improving the ensemble’s accuracy.
+# Ensemble methods work best when the predictors are as independent from one another as possible. One way to get diverse classifiers is to train them using very different algorithms.
+# This increases the chance that they will make very different types of errors, improving the ensemble’s accuracy.
 
 from sklearn import datasets
 from sklearn.svm import SVC
